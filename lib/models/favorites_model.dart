959a93ef24905840d08c8308ca99adb1bc761485
Hannan <sodrunberkicau@gmail.com>
2024-12-19 00:18:02 +0800

    refactor getter items in favorites model

--- lib/models/favorites_model.dart
+++ lib/models/favorites_model.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class FavoritesModel extends ChangeNotifier {
   final List<int> _favoriteItems = [];
 
-  List<int> get items => _favoriteItems;
+  List<int> get items {
+    return _favoriteItems;
+  }
 
   void add(int itemNo) {
     _favoriteItems.add(itemNo);
