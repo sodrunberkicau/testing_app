349c974c7d2e149b603cad1778ffbd82ef07b86f
Hannan <sodrunberkicau@gmail.com>
2024-12-19 00:19:01 +0800

    add debug log diagnostics when in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -9,7 +10,9 @@ void main() {
   runApp(const TestingApp());
 }
 
+// GoRouter digunakan untuk mengatur navigasi di flutter
 final _router = GoRouter(
+  debugLogDiagnostics: kDebugMode,
   routes: [
     GoRoute(
       path: HomeScreen.routeName,
