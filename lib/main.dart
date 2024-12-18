02404df99f2ef33e19c50c78e3116f4fdb0ef189
Hannan <sodrunberkicau@gmail.com>
2024-12-19 00:20:21 +0800

    Revert "add debug log diagnostics when in debug mode"
    This reverts commit 349c974c7d2e149b603cad1778ffbd82ef07b86f.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -10,9 +9,7 @@ void main() {
   runApp(const TestingApp());
 }
 
-// GoRouter digunakan untuk mengatur navigasi di flutter
 final _router = GoRouter(
-  debugLogDiagnostics: kDebugMode,
   routes: [
     GoRoute(
       path: HomeScreen.routeName,
